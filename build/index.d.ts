eed5e2fc91eaa33b9ee8d43c948c0178c8d55d9f
Harry Berg <harryaberg@gmail.com>
2020-11-12 14:41:12 +0000

    removed fragments which are not working

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,2 @@
 export * from './components/TestComponent';
 export * from "./components/Test";
-export * from './components/Form/Form';
--- src/components/Form/Fields/ConfirmPassword.jsx
+++ src/components/Form/Fields/ConfirmPassword.jsx
@@ -4,10 +4,10 @@ import TextResponse from "./TextField"
 
 const ConfirmPassword = props => {
     const [hidden, setHidden] = useState(true)
-    return <>
-        <Password {...props} title='Password' setHidden={()=>setHidden(!hidden)} />
+    return [
+        <Password {...props} title='Password' setHidden={()=>setHidden(!hidden)} />,
         <TextResponse {...props} type={hidden ? 'password' : 'text'} value={props.confirm_value} className="field" variant="outlined" id='confirm-password' title='Confirm password'/>
-    </>
+    ]
 }
 
 export default ConfirmPassword
